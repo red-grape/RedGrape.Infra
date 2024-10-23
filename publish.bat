556525c7c3ca59d9f6792fcef319f0338f74eca5
red-grape <theredgrape96@gmail.com>
2024-10-23 16:53:17 +0330

    revision #8

--- publish.bat
+++ publish.bat
@@ -1,4 +1,4 @@
-@echo off 
+rem @echo off 
 rem pack all project 
 dotnet pack ./RedGrape.Infra.Transmit/RedGrape.Infra.Transmit.csproj -c Release -o .  
 dotnet pack ./RedGrape.Infra.Core/RedGrape.Infra.Core.csproj -c Release -o .
@@ -6,7 +6,7 @@ dotnet pack ./RedGrape.Infra.Extentions/RedGrape.Infra.Extentions.csproj -c Rele
 
 rem Set the NuGet feed URL  
 set nuget_feed_url=https://api.nuget.org/v3/index.json  
-
+pause
 rem Loop through all .nupkg files in the current directory  
 for %%f in (*.nupkg) do (  
     rem Push the NuGet package to the feed  
